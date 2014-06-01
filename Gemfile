0cad18bf91a1d6b3e3f7ef4a981b86a98bb5ea58
Chris Keathley <spyc3r@gmail.com>
2014-06-01 14:48:04 -0400

    Add streaming endpoint for station infos

--- Gemfile
+++ Gemfile
@@ -2,7 +2,11 @@ source "https://rubygems.org"
 ruby "2.0.0"
 
 gem 'sinatra'
+gem 'sinatra-contrib'
 gem 'rack-cors'
+gem 'thin'
+gem 'eventmachine'
+gem 'em-http-request'
 
 group :development do
   gem 'foreman'
--- app.rb
+++ app.rb
@@ -2,7 +2,10 @@ require 'net/http'
 require 'uri'
 require 'json'
 require 'sinatra'
+require 'sinatra/streaming'
 require 'rack/cors'
+require 'eventmachine'
+require 'em-http-request'
 
 use Rack::Cors do |config|
   config.allow do |allow|
@@ -23,4 +26,17 @@ get "/api/stations" do
   request = Net::HTTP::Get.new(uri.request_uri)
   response = http.request(request)
   response.body.to_json
+end
+
+get "/api/stations/stream", :provides => 'text/event-stream' do
+  stream :keep_alive do |out|
+    EventMachine::PeriodicTimer.new(10) {
+      http = EventMachine::HttpRequest.new(
+        'http://www.bikechattanooga.com/stations/json'
+      ).get
+      http.callback {
+        out << "data: #{http.response}\n\n" unless out.closed?
+      }
+    }
+  end
 end
